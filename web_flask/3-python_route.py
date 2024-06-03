f8737100805a6cc25de9484c5ca151ced677eaf7
“Sylvanus <sylvanusuzor@gmail.com>
2024-06-02 20:13:53 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
-"""
+""" it Starts a flask web application
+ / displays sth and  /hbnb displays also"""
 
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+    """ Displays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Display 'HBNB'"""
+def display_hbnb():
+    """ Displays 'HBNB'"""
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """Display 'C ' followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays 'c' followed by the value of <text>"""
+    text = text.replace('_', ' ')
+    return "c {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text):
-    """Display 'Python ' followed by the value of the text variable"""
+def display_python_text(text='is cool'):
+    """Displays 'Python ' followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
