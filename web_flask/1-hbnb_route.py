5fdf6ab21a41a6db8741c9aaaafaf10c1d07e292
“Sylvanus <sylvanusuzor@gmail.com>
2024-06-02 19:57:21 -0700

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
-"""
+""" script that Starts a flask web application
+ displays sth and  /hbnb displays also"""
 
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+    """ Displays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Display 'HBNB'"""
+def display_hbnb():
+    """ Displays 'HBNB'"""
     return 'HBNB'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
