ce1edda302c50a15c5d928f8e84c52ae0f7939d2
“Sylvanus <sylvanusuzor@gmail.com>
2024-06-02 19:58:55 -0700

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+"""
+
+from web_flask import app
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Display 'C ' followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
